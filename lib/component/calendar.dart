ce98375e164929226595b4e0cb09c491f893b00d
jyr <jyr@wavers.co.kr>
2022-06-29 08:36:58 +0900

    Table Calendar 소개

---
+++ lib/component/calendar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class Calendar extends StatefulWidget {
+  const Calendar({Key? key}) : super(key: key);
+
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  DateTime? selectedDay;
+
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      focusedDay: DateTime.now(),
+      firstDay: DateTime(1800),
+      lastDay: DateTime(3000),
+      headerStyle: HeaderStyle(
+        formatButtonVisible: false,
+        titleCentered: true,
+        titleTextStyle: TextStyle(
+          fontWeight: FontWeight.w700,
+          fontSize: 16.0,
+        ),
+      ),
+      onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+        print(selectedDay);
+        setState(() {
+          this.selectedDay = selectedDay;
+        });
+      },
+      selectedDayPredicate: (DateTime date) {
+        print('===== date :: $date =====');
+        print('===== selectedDay :: $selectedDay =====');
+        if (selectedDay == null) {
+          return false;
+        }
+        return date.year == selectedDay!.year &&
+            date.month == selectedDay!.month &&
+            date.day == selectedDay!.day;
+      },
+    );
+  }
+}
